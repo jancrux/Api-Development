ed3679669ece8ff76a6f4754b0468e8a0d4949e6
José <jancrux@gmail.com>
2026-02-06 14:53:09 +0000

    Models e Data

---
+++ Api_Development/Data/ApiContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Api_Development.Models;
+
+
+namespace Api_Development.Data
+{
+    public class ApiContext : DbContext
+    {
+
+        public DbSet<HotelBooking> Bookings { get; set; }
+
+        public ApiContext(DbContextOptions<ApiContext> options)
+            :base(options) { 
+        
+        
+        }
+    }
+}
---
+++ Api_Development/Models/HotelBooking.cs
@@ -0,0 +1,11 @@
+﻿namespace Api_Development.Models
+{
+    public class HotelBooking
+    {
+        public string Id { get; set; }
+
+        public string RoomNumber { get; set; }
+
+        public string? ClientName { get; set; }
+    }
+}
